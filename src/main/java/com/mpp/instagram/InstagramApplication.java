b77cf046f8f0ab1ad23065954c60be1a31c7b279
Adnan Shehzad <adnanshahzad00003@gmail.com>
2019-10-10 07:02:07 +0800

    AbuBakar Code
    Resolved Issue

--- src/main/java/com/mpp/instagram/InstagramApplication.java
+++ src/main/java/com/mpp/instagram/InstagramApplication.java
@@ -1,15 +1,17 @@
 package com.mpp.instagram;
 
+import com.mpp.instagram.storage.StorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
-//@EntityScan("com.mpp.instagram.user.repository.UserRepository")
-//@EnableCassandraRepositories("com.mpp.instagram.user.entity")
+@EnableConfigurationProperties({StorageProperties.class})
 public class InstagramApplication {
 
     public static void main(String[] args) {
